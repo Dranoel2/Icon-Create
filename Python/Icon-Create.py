e0545f271f0a994ba2bd7eb216d6f338e8f9e9cd
dranoel1812 <45979197+dranoel1812@users.noreply.github.com>
2020-03-01 10:13:07 +0000

    Working?

--- Python/Icon-Create.py
+++ Python/Icon-Create.py
@@ -1,3 +1,17 @@
 from PIL import Image
 
+sizes = [512,128,32,16]
+
+name = input('Name of icns file?')
+
 from resizeimage import resizeimage
+
+with open('..Graphics/icon.png', 'r+b') as icon:
+    with Image.open(icon) as image:
+        for size in sizes:
+            x = size*2
+            final = resizeimage.resize_contain(image, [x,x])
+            file.save('..{}.iconset/icon_{}x{}@x2.png'.format(name, x, x), image.format)
+            x = size
+            final = resizeimage.resize_contain(image, [x,x])
+            file.save('..{}.iconset/icon_{}x{}.png'.format(name, x, x), image.format)
